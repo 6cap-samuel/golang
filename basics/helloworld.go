a7ee57234dc95efa3482a6bcb8a5b8e096759070
Samuel! :) <29274144+6cap-samuel@users.noreply.github.com>
2021-12-17 08:53:33 +0800

    [INTRO] Setting up and adding in hello world sample code.

---
+++ basics/helloworld.go
@@ -0,0 +1,15 @@
+package main
+
+// single line comment
+import (
+	"fmt"
+)
+
+/*
+	multi block comment
+	Main: entry point of the each go file
+*/
+func main() {
+	// tabs and semi colon is not a must but recommended
+	fmt.Println("Hello World")
+}
